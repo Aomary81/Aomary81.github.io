2bb5c683ddc3c0521512c6f3e699b81705552271
Aomary81 <89872670+Aomary81@users.noreply.github.com>
2022-10-27 16:01:30 -0700

    Assignment 4
    Added the fancify html and js files

---
+++ fancifymytext.js
@@ -0,0 +1,33 @@
+function bigger() {
+  var textarea = document.getElementById("textarea");
+  textarea.style.fontSize = "24pt";
+  hello();
+}
+
+function fancy() {
+  var clicked = document.getElementById("fancy");
+  var textarea = document.getElementById("textarea");
+  if (clicked.checked) {
+    hello();
+    textarea.style.fontWeight = "bold";
+    textarea.style.color = "blue";
+    textarea.style.textDecoration = "underline";
+  } else {
+    hello();
+    textarea.style.fontWeight = "normal";
+    textarea.style.color = "black";
+    textarea.style.textDecoration = "none";
+  }
+}
+
+function hello() {
+  alert("Hello, World!");
+}
+
+function moo() {
+  var textarea = document.getElementById("textarea");
+  textarea.style.textTransform = "uppercase";
+  var period = textarea.value.split(".");
+  textarea.value = period.join("-Moo");
+  hello();
+}
